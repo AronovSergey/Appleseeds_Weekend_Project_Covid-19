2699177868696fdec3c364f4643594cc7e3f7593
Sergey <aronovsergeyy@gmail.com>
2021-03-04 09:53:53 +0200

    Creating an object that transfers a country to a region

---
+++ script/countriesApiImplementation.js
@@ -0,0 +1,19 @@
+const baseEndpoint = 'https://restcountries.herokuapp.com/api/v1';
+const proxy = 'https://api.allorigins.win/raw?url';
+
+
+async function getAllTheCountries() {
+    const response = await fetch(`${proxy}=${baseEndpoint}`);
+
+    const data = await response.json();
+
+    const countryToRegion = {};
+
+    for(let i = 0; i < data.length; i++)
+        countryToRegion[data[i].name.common] = data[i].region;
+
+
+    return countryToRegion;
+}
+
+getAllTheCountries();
